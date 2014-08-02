067e5dfbbc89bf7791f0e6c0bdbfe54bb2028b99
Anuvind V. Menon <anuvind@leaf.me>
2014-08-02 12:20:15 -0400

    create video model and establish relationship with user

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802153851) do
+ActiveRecord::Schema.define(version: 20140802160904) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "firstname"
     t.string   "lastname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "videos", force: true do |t|
+    t.string   "name"
+    t.text     "context"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
